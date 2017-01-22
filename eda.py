3239aa7da638b2702070a6bd85c5841d8afc3582
ergindemir <mrergin@gmail.com>
2017-01-21 16:23:44 -0800

    convert csv to pickle

---
+++ eda.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#df = pd.read_csv('data/loan.csv',low_memory=False)
+#df.to_pickle('data/loan.pkl')
+
+df=pd.read_pickle('data/loan.pkl')
+
+
