c3baae25443da6d58eb7a5ac6bd30a66b68cb22e
ergindemir <mrergin@gmail.com>
2017-01-22 15:32:43 -0800

    date columns parsed

--- eda.py
+++ eda.py
@@ -8,4 +8,15 @@ import matplotlib.pyplot as plt
 
 df=pd.read_pickle('data/loan.pkl')
 
+df['issueDate'] = pd.to_datetime(df.issue_d,format='%b-%Y')
+df['lastPaymentDate'] = pd.to_datetime(df.last_pymnt_d,format='%b-%Y')
+df['lastCreditPullDate'] = pd.to_datetime(df.last_credit_pull_d,format='%b-%Y')
 
+bad_status = ['Charged Off',
+ 'Default',
+ 'Late (31-120 days)',
+ 'In Grace Period',
+ 'Late (16-30 days)',
+ 'Does not meet the credit policy. Status:Charged Off']
+
+df['default'] = df.loan_status.apply(lambda x: 1 if x in bad_status else 0)
