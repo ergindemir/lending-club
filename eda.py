7ce540098821304ca02dc7088cd712b53e8f728b
ergindemir <mrergin@gmail.com>
2017-01-22 23:11:20 -0800

    more eda

--- eda.py
+++ eda.py
@@ -20,3 +20,33 @@ bad_status = ['Charged Off',
  'Does not meet the credit policy. Status:Charged Off']
 
 df['default'] = df.loan_status.apply(lambda x: 1 if x in bad_status else 0)
+df['termLength'] = df.term.apply(lambda x: int(x.strip().split()[0]))
+
+#with open(r"data/df.pkl", "wb") as output_file:
+#     cPickle.dump(df, output_file)
+#
+#now=datetime.datetime.now()
+#with open(r"data/loan.pkl", "rb") as output_file: 
+#    df3 = cPickle.load(output_file)
+#print datetime.datetime.now()-now
+#
+#now=datetime.datetime.now()
+#df3=pd.read_pickle('data/loan.pkl')
+#print datetime.datetime.now()-now
+
+emp_length_values = df.groupby(df.emp_length).default.mean().sort_values().index                           
+emp_length_dict=dict(zip(emp_length_values,range(len(emp_length_values))))
+df['empLength'] = df.emp_length.apply(lambda x: emp_length_dict[x])
+
+home_ownership_values = df.groupby(df.home_ownership).default.mean().sort_values().index
+home_ownership_dict = dict(zip(home_ownership_values,range(len(home_ownership_values))))
+df['homeOwnership'] = df.home_ownership.apply(lambda x: home_ownership_dict[x])
+
+df['verificationStatus'] = df.verification_status.apply(lambda x:1 if x in ['Verified', 'Source Verified'] else 0)
+
+
+
+
+
+
+
